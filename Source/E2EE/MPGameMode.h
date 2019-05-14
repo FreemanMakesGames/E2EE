3b05d37f8ac3128fdf4df285866392342f915a39
Freeman <freeman@freemanmakesgames.pro>
2019-05-14 04:08:37 -0700

    New necessary classes are created. * Home Menu gives up setting Game Mode for Level0. Instead, Level0_SP and Level0_MP are created, because the levels indeed are different from each other.

---
+++ Source/E2EE/MPGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class E2EE_API AMPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
