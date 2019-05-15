eb82e4236a94d3af21480115ebb984cd4bc54c9b
Freeman <freeman@freemanmakesgames.pro>
2019-05-15 01:12:43 -0700

    The RPC to submit character selection request is put into BP_MyPlayerController.

--- Source/E2EE/MPGameMode.cpp
+++ Source/E2EE/MPGameMode.cpp
@@ -1,2 +1,8 @@
 #include "MPGameMode.h"
 
+bool AMPGameMode::ProcessCharacterSelectionRequest( AMyPlayerController* PlayerCtrl, FString CharacterName )
+{
+	UE_LOG( LogTemp, Warning, TEXT( "Request is received." ) );
+
+	return false;
+}
--- Source/E2EE/MPGameMode.h
+++ Source/E2EE/MPGameMode.h
@@ -4,6 +4,9 @@
 #include "GameFramework/GameModeBase.h"
 #include "MPGameMode.generated.h"
 
+class AMyPlayerController;
+class AE2EECharacter;
+
 /**
  * 
  */
@@ -11,5 +14,27 @@ UCLASS()
 class E2EE_API AMPGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY( VisibleInstanceOnly, BlueprintReadWrite )
+	AMyPlayerController* AliceBobCtrl;
+
+	UPROPERTY( VisibleInstanceOnly, BlueprintReadWrite )
+	AMyPlayerController* MessengerCtrl;
+
+	UPROPERTY( VisibleInstanceOnly, BlueprintReadWrite )
+	AE2EECharacter* Alice;
+
+	UPROPERTY( VisibleInstanceOnly, BlueprintReadWrite )
+	AE2EECharacter* Bob;
+
+	UPROPERTY( VisibleInstanceOnly, BlueprintReadWrite )
+	AE2EECharacter* Messenger;
+
+public:
+
+	UFUNCTION( BlueprintCallable )
+	bool ProcessCharacterSelectionRequest( AMyPlayerController* PlayerCtrl, FString CharacterName );
 	
 };
