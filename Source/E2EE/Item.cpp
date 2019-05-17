c62e428f1a314a74996d453e0a1bdddd1dc5f38b
Freeman <freeman@freemanmakesgames.pro>
2019-05-16 21:17:16 -0700

    AItem's bReplicates is set to true. Lock, Key, and Container replicate movement.

--- Source/E2EE/Item.cpp
+++ Source/E2EE/Item.cpp
@@ -6,6 +6,8 @@ AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
 }
 
 // Called when the game starts or when spawned
