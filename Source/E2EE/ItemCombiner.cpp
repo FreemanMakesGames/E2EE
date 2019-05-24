e92f37aaae63d39475ca08f00d95e557e8d15ebd
Freeman <freeman@freemanmakesgames.pro>
2019-05-24 03:26:51 -0700

    C++ Item Combiner is attempted again, and it compiles.

--- Source/E2EE/ItemCombiner.cpp
+++ Source/E2EE/ItemCombiner.cpp
@@ -1,81 +1,84 @@
-// #include "ItemCombiner.h"
-// 
-// #include "Item.h"
-// 
-// #include "UObject/ConstructorHelpers.h"
-// #include "Templates/Function.h"
-// 
-// UItemCombiner::UItemCombiner()
-// {
-// 	/* All classes of combinable Items */
-// 
-// 	static ConstructorHelpers::FClassFinder<AItem> LockClassFinder( TEXT( "/Game/Items/Blueprints/BP_Lock" ) );
-// 	TSubclassOf<AItem> LockClass = LockClassFinder.Class;
-// 	
-// 	static ConstructorHelpers::FClassFinder<AItem> KeyClassFinder( TEXT( "/Game/Items/Blueprints/BP_Key" ) );
-// 	TSubclassOf<AItem> KeyClass = KeyClassFinder.Class;
-// 	
-// 	static ConstructorHelpers::FClassFinder<AItem> ContainerClassFinder( TEXT( "/Game/Items/Blueprints/BP_Container" ) );
-// 	TSubclassOf<AItem> ContainerClass = ContainerClassFinder.Class;
-// 	
-// 	static ConstructorHelpers::FClassFinder<AItem> MessageClassFinder( TEXT( "/Game/Items/Blueprints/BP_Message" ) );
-// 	TSubclassOf<AItem> MessageClass = MessageClassFinder.Class;
-// 
-// 	/* All possible combination among Item types */
-// 
-// 	TArray<TSubclassOf<AItem>> LockAndContainer;
-// 	LockAndContainer.Add( LockClass );
-// 	LockAndContainer.Add( ContainerClass );
-// 	FArrayOfItemClasses LockAndContainerArray( LockAndContainer );
-// 
-// 	TArray<TSubclassOf<AItem>> KeyAndContainer;
-// 	KeyAndContainer.Add( KeyClass );
-// 	KeyAndContainer.Add( ContainerClass );
-// 
-// 	TArray<TSubclassOf<AItem>> ContainerAndLock;
-// 	ContainerAndLock.Add( ContainerClass );
-// 	ContainerAndLock.Add( LockClass );
-// 
-// 	TArray<TSubclassOf<AItem>> ContainerAndKey;
-// 	ContainerAndKey.Add( ContainerClass );
-// 	ContainerAndKey.Add( KeyClass );
-// 
-// 	TArray<TSubclassOf<AItem>> ContainerAndMessage;
-// 	ContainerAndMessage.Add( ContainerClass );
-// 	ContainerAndMessage.Add( MessageClass );
-// 
-// 	/* Assign functions. */
-// // 
-// // 	LockContainerFunction.BindUObject( this, &UItemCombiner::LockContainer );
-// // 
-// // 	UnlockContainerFunction.BindUObject( this, &UItemCombiner::UnlockContainer );
-// 
-// 	/* Populate the function map. */
-// 
-// 	FunctionIndexMap.Add( LockAndContainerArray, 0 );
-// }
-// 
-// void UItemCombiner::CombineItems()
-// {
-// 
-// }
-// 
-// void UItemCombiner::SetSourceItems( TArray<AItem*> Items )
-// {
-// 	SourceItems = Items;
-// }
-// 
-// TArray<AItem*> UItemCombiner::GetResultItems()
-// {
-// 	return ResultItems;
-// }
-// 
-// void UItemCombiner::LockContainer()
-// {
-// 
-// }
-// 
-// void UItemCombiner::UnlockContainer()
-// {
-// 
-// }
+#include "ItemCombiner.h"
+
+#include "Item.h"
+#include "ItemInfo.h"
+#include "DevUtilities.h"
+
+#include "UObject/ConstructorHelpers.h"
+#include "Templates/Function.h"
+
+UItemCombiner::UItemCombiner()
+{
+	/* All classes of combinable Items */
+
+	static ConstructorHelpers::FClassFinder<AItem> LockClassFinder( TEXT( "/Game/Items/Blueprints/BP_Lock" ) );
+	TSubclassOf<AItem> LockClass = LockClassFinder.Class;
+
+	static ConstructorHelpers::FClassFinder<AItem> KeyClassFinder( TEXT( "/Game/Items/Blueprints/BP_Key" ) );
+	TSubclassOf<AItem> KeyClass = KeyClassFinder.Class;
+
+	static ConstructorHelpers::FClassFinder<AItem> ContainerClassFinder( TEXT( "/Game/Items/Blueprints/BP_Container" ) );
+	TSubclassOf<AItem> ContainerClass = ContainerClassFinder.Class;
+
+	static ConstructorHelpers::FClassFinder<AItem> MessageClassFinder( TEXT( "/Game/Items/Blueprints/BP_Message" ) );
+	TSubclassOf<AItem> MessageClass = MessageClassFinder.Class;
+
+	/* All possible combination among Item types */
+
+	TArray<TSubclassOf<AItem>> LockAndContainer;
+	LockAndContainer.Add( LockClass );
+	LockAndContainer.Add( ContainerClass );
+	FArrayOfItemClasses ArrayOfLockAndContainerClasses( LockAndContainer );
+	FunctionMap.Add( ArrayOfLockAndContainerClasses, &UItemCombiner::LockContainer );
+
+	TArray<TSubclassOf<AItem>> KeyAndContainer;
+	KeyAndContainer.Add( KeyClass );
+	KeyAndContainer.Add( ContainerClass );
+	FArrayOfItemClasses ArrayOfKeyAndContainerClasses( KeyAndContainer );
+	FunctionMap.Add( ArrayOfKeyAndContainerClasses, &UItemCombiner::UnlockContainer );
+
+	TArray<TSubclassOf<AItem>> ContainerAndLock;
+	ContainerAndLock.Add( ContainerClass );
+	ContainerAndLock.Add( LockClass );
+
+	TArray<TSubclassOf<AItem>> ContainerAndKey;
+	ContainerAndKey.Add( ContainerClass );
+	ContainerAndKey.Add( KeyClass );
+
+	TArray<TSubclassOf<AItem>> ContainerAndMessage;
+	ContainerAndMessage.Add( ContainerClass );
+	ContainerAndMessage.Add( MessageClass );
+}
+
+TArray<UItemInfo*> UItemCombiner::CombineItems(TArray<UItemInfo*> SourceItems)
+{
+	FArrayOfItemClasses SourceItemClasses;
+	for ( int i = 0; i < SourceItems.Num(); i++ )
+	{
+		SourceItemClasses.ItemClasses.Add( SourceItems[i]->GetClass() );
+	}
+
+	TArray<UItemInfo*> Results;
+
+	FCombineFunction CombineFunction = *( FunctionMap.Find( SourceItemClasses ) );
+	if ( CombineFunction )
+	{
+		Results = ( this->*( CombineFunction ) )( SourceItems );
+	}
+
+	return Results;
+}
+
+TArray<UItemInfo*> UItemCombiner::LockContainer( TArray<UItemInfo*> SourceItems )
+{
+	UDevUtilities::PrintInfo( "Lock container!" );
+
+	return SourceItems;
+}
+
+TArray<UItemInfo*> UItemCombiner::UnlockContainer( TArray<UItemInfo*> SourceItems )
+{
+	UDevUtilities::PrintInfo( "Unlock container!" );
+
+	return SourceItems;
+}
--- Source/E2EE/ItemCombiner.h
+++ Source/E2EE/ItemCombiner.h
@@ -1,105 +1,87 @@
-// #pragma once
-// 
-// #include "CoreMinimal.h"
-// #include "UObject/NoExportTypes.h"
-// #include "ItemCombiner.generated.h"
-// 
-// class AItem;
-// 
-// DECLARE_DELEGATE( FCombineFunction );
-// 
-// USTRUCT()
-// struct FArrayOfItemClasses
-// {
-// 	GENERATED_BODY()
-// 
-// public:
-// 
-// 	FArrayOfItemClasses( TArray<TSubclassOf<AItem>> Items )
-// 	{
-// 		this->Items = Items;
-// 	}
-// 
-// 	UPROPERTY()
-// 	TArray<TSubclassOf<AItem>> Items;
-// 
-// 	bool operator==( const FArrayOfItemClasses& Other ) const
-// 	{
-// 		return Items == Other.Items;
-// 	}
-// };
-// 
-// FORCEINLINE uint32 GetTypeHash( const FArrayOfItemClasses& ArrayOfItems )
-// {
-// 	return FCrc::MemCrc_DEPRECATED( &ArrayOfItems, sizeof( FArrayOfItemClasses ) );
-// }
-// 
-// /**
-//  * 
-//  */
-// UCLASS( BlueprintType )
-// class E2EE_API UItemCombiner : public UObject
-// {
-// 	GENERATED_BODY()
-// 
-// public:
-// 
-// 	UItemCombiner();
-// 
-// public:
-// 
-// 	UFUNCTION( BlueprintCallable )
-// 	void CombineItems();
-// 
-// // Blueprint classes
-// // protected:
-// // 	
-// // 	UPROPERTY( EditDefaultsOnly )
-// // 	TSubclassOf<AItem> LockClass;
-// // 	
-// // 	UPROPERTY( EditDefaultsOnly )
-// // 	TSubclassOf<AItem> KeyClass;
-// // 	
-// // 	UPROPERTY( EditDefaultsOnly )
-// // 	TSubclassOf<AItem> ContainerClass;
-// // 	
-// // 	UPROPERTY( EditDefaultsOnly )
-// // 	TSubclassOf<AItem> MessageClass;
-// 
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemCombiner.generated.h"
+
+class AItem;
+class UItemInfo;
+
+USTRUCT()
+struct FArrayOfItemClasses
+{
+	GENERATED_BODY()
+
+public:
+
+	FArrayOfItemClasses() {}
+
+	FArrayOfItemClasses( TArray<TSubclassOf<AItem>> ItemClasses )
+	{
+		this->ItemClasses = ItemClasses;
+	}
+
+	UPROPERTY()
+	TArray<TSubclassOf<AItem>> ItemClasses;
+
+	bool operator==( const FArrayOfItemClasses& Other ) const
+	{
+		return ItemClasses == Other.ItemClasses;
+	}
+};
+
+FORCEINLINE uint32 GetTypeHash( const FArrayOfItemClasses& ArrayOfItems )
+{
+	return FCrc::MemCrc_DEPRECATED( &ArrayOfItems, sizeof( FArrayOfItemClasses ) );
+}
+
+/**
+ * 
+ */
+UCLASS( BlueprintType )
+class E2EE_API UItemCombiner : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	typedef TArray<UItemInfo*>( UItemCombiner::*FCombineFunction )( TArray <UItemInfo*> );
+
+public:
+
+	UItemCombiner();
+
+// Blueprint classes
 // protected:
-// 
-// // 	UPROPERTY( VisibleAnywhere )
-// // 	TMap<FArrayOfItems, TFunction<void()>> FunctionMap;
-// 
-// 	UPROPERTY( VisibleAnywhere )
-// 	TMap<FArrayOfItemClasses, int> FunctionIndexMap;
-// 
-// public:
-// 
-// 	void SetSourceItems( TArray<AItem*> Items );
-// 
-// 	TArray<AItem*> GetResultItems();
-// 
-// protected:
-// 
-// 	UPROPERTY()
-// 	TArray<AItem*> SourceItems;
-// 
-// 	UPROPERTY()
-// 	TArray<AItem*> ResultItems;
-// 
-// // protected:
-// // 
-// // 	FCombineFunction LockContainerFunction;
-// // 
-// // 	FCombineFunction UnlockContainerFunction;
-// 
-// protected:
-// 
-// 	UFUNCTION( BlueprintCallable )
-// 	void LockContainer();
-// 
-// 	UFUNCTION( BlueprintCallable )
-// 	void UnlockContainer();
 // 	
-// };
+// 	UPROPERTY( EditDefaultsOnly )
+// 	TSubclassOf<AItem> LockClass;
+// 	
+// 	UPROPERTY( EditDefaultsOnly )
+// 	TSubclassOf<AItem> KeyClass;
+// 	
+// 	UPROPERTY( EditDefaultsOnly )
+// 	TSubclassOf<AItem> ContainerClass;
+// 	
+// 	UPROPERTY( EditDefaultsOnly )
+// 	TSubclassOf<AItem> MessageClass;
+
+protected:
+
+	TMap<FArrayOfItemClasses, FCombineFunction> FunctionMap;
+	//FCombineFunction CombineFunctions[30];
+
+public:
+
+	UFUNCTION( BlueprintCallable )
+	TArray<UItemInfo*> CombineItems( TArray<UItemInfo*> SourceItems );
+
+protected:
+
+	UFUNCTION( BlueprintCallable )
+	TArray<UItemInfo*> LockContainer( TArray<UItemInfo*> SourceItems );
+
+	UFUNCTION( BlueprintCallable )
+	TArray<UItemInfo*> UnlockContainer( TArray<UItemInfo*> SourceItems );
+	
+};
